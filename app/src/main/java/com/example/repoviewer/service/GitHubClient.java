9d823b7a64601b640b6a273cf2a554219f3ef830
jiajie <jiajie.wu96@gmail.com>
2019-12-05 17:26:50 -0800

    Call propery gets a response

--- app/src/main/java/com/example/repoviewer/service/GitHubClient.java
+++ app/src/main/java/com/example/repoviewer/service/GitHubClient.java
@@ -8,10 +8,13 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface GitHubClient {
+    @Headers("Accept: application/json")
     @POST("login/oauth/access_token")
     @FormUrlEncoded
     Call<AccessToken> getAccessToken(
@@ -19,6 +22,6 @@ public interface GitHubClient {
             @Field("client_secret") String clientSecret,
             @Field("code") String code
     );
-    @GET("/users/{user}/repos")
-    Call<List<>>reposForUser(@Path("user") String user);
+//    @GET("/users/{user}/repos")
+//    Call<List<>>reposForUser(@Path("user") String user);
 }
--- app/src/main/java/com/example/repoviewer/ui/LoginActivity.java
+++ app/src/main/java/com/example/repoviewer/ui/LoginActivity.java
@@ -5,39 +5,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.repoviewer.BuildConfig;
 import com.example.repoviewer.R;
+import com.example.repoviewer.data.model.AccessToken;
+import com.example.repoviewer.service.GitHubClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
     private String mGithubOauthURL = "https://github.com/login/oauth/authorize";
     private String mGithubClientId = BuildConfig.GITHUB_CLIENT_ID;
     private String mGithubSecret = BuildConfig.GITHUB_SECRET;
-    private String callbackUrl = "http://example.com/someresource/";
+    private String callbackUrl = "http://example.com/path/";
     private Uri mUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         mUri = getIntent().getData();
-        if (mUri == null || !mUri.toString().startsWith(callbackUrl)) {
-            Uri githubOauthUrl = Uri.parse(mGithubOauthURL+"?client_id="+mGithubClientId+"&scope=repo&redirect_uri="+callbackUrl);
+        if(mUri == null) {
+            Uri githubOauthUrl = Uri.parse(mGithubOauthURL + "?client_id=" + mGithubClientId + "&scope=repo&redirect_uri=" + callbackUrl);
             Intent intent = new Intent(Intent.ACTION_VIEW, githubOauthUrl);
             startActivity(intent);
         }
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mUri == null || !mUri.toString().startsWith(callbackUrl)) {
-            mUri = getIntent().getData();
-        }
-        if( mUri != null && mUri.toString().startsWith(callbackUrl)){
+
+        mUri = getIntent().getData();
+        if (mUri != null && mUri.toString().startsWith(callbackUrl)) {
             String code = mUri.getQueryParameter("code");
+            buildClientCall(code);
         }
+
+    }
+
+    private void buildClientCall(String code) {
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://github.com/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        GitHubClient gitHubClient = retrofit.create(GitHubClient.class);
+        Call<AccessToken> accessToken = gitHubClient.getAccessToken(
+                mGithubClientId,
+                mGithubSecret,
+                code
+        );
+
+        accessToken.enqueue(new Callback<AccessToken>() {
+            @Override
+            public void onResponse(Call<AccessToken> call, Response<AccessToken> response) {
+                Toast.makeText(LoginActivity.this, "yay!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<AccessToken> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "boo!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
